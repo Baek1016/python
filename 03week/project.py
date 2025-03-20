5c2e5f8270f4cf4b0e07db5810005778c615d488
BAEKJAEWON <baejjae1016@gmail.com>
2025-03-20 15:00:39 +0000

    123123

--- 03week/project.py
+++ 03week/project.py
@@ -1,7 +1,9 @@
 a = int(input("입력 진수 결정(16/10/8/2)"))
 b = input("값 입력")
 
-print(f"16진수 ==> {hex(b)}")
-print(f"10진수 ==> (b)")
-print(f" 8진수 ==> {oct(b)}")
-print(f" 2진수 ==> {bin(b)}")
+c= int(b, a)
+
+print(f"16진수 ==> {hex(c)}")
+print(f"10진수 ==> {c}")
+print(f" 8진수 ==> {oct(c)}")
+print(f" 2진수 ==> {bin(c)}")
