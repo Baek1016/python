f620eb11cdb53a7888f3b1c7f6e7d6a41677e110
BAEKJAEWON <baejjae1016@gmail.com>
2025-05-11 07:42:03 +0000

    함수 정의 주석 처리 및 multi 함수 구현: 두 수의 합과 차를 계산하여 반환

--- Study_python02/5_11_02.py
+++ Study_python02/5_11_02.py
@@ -11,15 +11,49 @@
 # func1()
 # func2()
 
-def func1():
-    result = 100
-    return result
+# def func1():
+#     result = 100
+#     return result
 
-def func2():
-    print("반환값이 없는 함수 실행")
+# def func2():
+#     print("반환값이 없는 함수 실행")
 
-hap = 0
+# hap = 0
 
-hap = func1() # func1()의 반환값을 hap에 저장
-print("func1()의 반환값 : %d" % hap)
-func2() # 반환값이 없는 함수 실행
+# hap = func1() # func1()의 반환값을 hap에 저장
+# print("func1()의 반환값 : %d" % hap)
+# func2() # 반환값이 없는 함수 실행
+
+# def func1(x):
+#     x+=1
+#     print("1증가 했습니다.")
+#     return x
+
+# def func2(x):
+#     x+=1
+#     print("1증가 했습니다.")
+
+# a= 10 
+
+# a= func1(a)
+# print("func1()의 반환값 : %d" % a)
+
+# func2(a)
+# print("func2()의 반환값 : %d" % a)
+
+
+def multi(v1,v2):
+    retList = []
+    res1 = v1 +v2
+    res2 = v1 -v2
+    retList.append(res1)
+    retList.append(res2)
+    return retList
+
+myList= []
+hap, sub=(0,0)
+myList = multi(10,20)
+hap = myList[0]
+sub = myList[1]
+
+print("multi()돌려준값 ==> %d,%d" % (hap, sub))
